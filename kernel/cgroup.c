f0f87db4678c0f0f45ba7db8f0b6405021d3bfe5
Chris Fries <C.Fries@motorola.com>
2013-03-19 14:24:32 -0500

    cgroup: Fix uninitialized 'it' use
    Change-Id: I3d64e2330189a6493e9cb265c02b39f6b0d9c38f
    Signed-off-by: Chris Fries <C.Fries@motorola.com>
    (cherry picked from commit 43d581b64bf704680811b5315b7f1c325c75a16a)

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2256,9 +2256,9 @@ static inline int started_after(void *p1, void *p2)
 int cgroup_scan_tasks(struct cgroup_scanner *scan)
 {
 	int retval, i;
-	struct cgroup_iter it;
-	struct task_struct *p, *dropped;
-	
+	struct cgroup_iter it = {0, 0};
+ 	struct task_struct *p, *dropped;
+ 	/* Never dereference latest_task, since it's not refcounted */
 	struct task_struct *latest_task = NULL;
 	struct ptr_heap tmp_heap;
 	struct ptr_heap *heap;
