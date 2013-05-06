c24c64bab3bbf237c6e5a77a7144d5e43d2c25ce
Colin Cross <ccross@android.com>
2013-05-06 23:50:08 +0000

    lockdep: remove task argument from debug_check_no_locks_held
    The only existing caller to debug_check_no_locks_held calls it
    with 'current' as the task, and the freezer needs to call
    debug_check_no_locks_held but doesn't already have a current
    task pointer, so remove the argument.  It is already assuming
    that the current task is relevant by dumping the current stack
    trace as part of the warning.
    
    This was originally part of 6aa9707099c (lockdep: check that
    no locks held at freeze time) which was reverted in
    dbf520a9d7d4.
    
    Original-author: Mandeep Singh Baines <msb@chromium.org>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -823,7 +823,10 @@ void do_exit(long code)
 	if (unlikely(current->pi_state_cache))
 		kfree(current->pi_state_cache);
 #endif
-	debug_check_no_locks_held(tsk);
+	/*
+	 * Make sure we are holding no locks:
+	 */
+	debug_check_no_locks_held();
 	tsk->flags |= PF_EXITPIDONE;
 
 	if (tsk->io_context)
