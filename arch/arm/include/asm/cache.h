f53cd1f1498f9cdbde7a25f51e9ef5e6f6816aee
Harm Hanemaaijer <fgenfb@yahoo.com>
2013-07-12 02:13:14 +0200

    Optimize copy_page for modern ARM platforms
    The existing implementation of copy_page for ARM appears to be
    optimized for older platforms. Benchmark testing in a sandbox
    environment shows suboptimal performance on modern platforms
    like armv6 and armv7, with speed-ups ranging from 10% (Cortex A8)
    to 80% (armv6 used in Raspberry Pi) being achievable.
    
    This commit optimizes copy_page and introduces the new compile-time
    constant PREFETCH_DISTANCE, defined in cache.h, which when
    multiplied by L1_CACHE_BYTES is equal to the offset used for
    prefetches performed with the PLD instruction. For platforms where
    L1_CACHE_BYTES is 32 (armv5 and armv6), copy_page processes 32 bytes
    at a time while doing one prefetch per iteration, while for armv7
    (with L1_CACHE_BYTES equal to 64), 64 bytes are processed at at time
    with one prefetch per iteration. When no preload instruction is
    available (platforms earlier than armv5), no preload instructions
    are generated and 32 bytes are processed at at time.
    
    To facilitate specifying instructions for architectures with no
    preload instruction, the NO_PLD macro is added to assembler.h,
    augmenting the PLD macro.
    
    Signed-off-by: Harm Hanemaaijer <fgenfb@yahoo.com>
    Signed-off-by: poondog <markj338@gmail.com>

--- arch/arm/include/asm/cache.h
+++ arch/arm/include/asm/cache.h
@@ -4,6 +4,29 @@
 #define L1_CACHE_SHIFT		CONFIG_ARM_L1_CACHE_SHIFT
 #define L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
 
+/*
+ * Set the prefetch distance in units of L1_CACHE_BYTES based on the
+ * cache line size. The prefetch distance is used by the memcpy,
+ * copy_from_user, copy_to_user versions that are optimized
+ * for ARM v6 and v7 platforms, as well as the copy_page function
+ * on ARM v5, v6 and v7 platforms.
+ */
+
+#if L1_CACHE_BYTES == 64
+/*
+ * This value was calibrated on a Cortex A8-based SOC with a 32-bit
+ * DDR3 interface. Other Cortex cores and architectures may benefit
+ * from a different setting.
+ */
+#define PREFETCH_DISTANCE 3
+#else
+/*
+ * This value was calibrated on the ARM v6-based SOC used in the Raspbery
+ * Pi. Other architectures may benefit from a different setting.
+ */
+#define PREFETCH_DISTANCE 3
+#endif
+
 #define ARCH_DMA_MINALIGN	L1_CACHE_BYTES
 
 #if defined(CONFIG_AEABI) && (__LINUX_ARM_ARCH__ >= 5)
