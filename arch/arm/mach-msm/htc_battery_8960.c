18a464ff948327b3b7dd33714f72d977f2e14be2
flar2 <asegaert@gmail.com>
2013-11-23 00:42:50 -0500

    Fix USB FAST CHARGE for HTC battery drivers
    Signed-off-by: poondog <markj338@gmail.com>

--- arch/arm/mach-msm/htc_battery_8960.c
+++ arch/arm/mach-msm/htc_battery_8960.c
@@ -36,6 +36,10 @@
 #include <linux/suspend.h>
 #include <linux/earlysuspend.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #include <mach/htc_gauge.h>
 #include <mach/htc_charger.h>
 #include <mach/htc_battery_cell.h>
@@ -443,7 +447,13 @@ int htc_charger_event_notify(enum htc_charger_event event)
 		htc_batt_schedule_batt_info_update();
 		break;
 	case HTC_CHARGER_EVENT_SRC_USB: 
-		latest_chg_src = CHARGER_USB;
+		if (force_fast_charge == 1) {
+			printk("[FASTCHARGE] Forcing CHARGER_AC");
+			latest_chg_src = CHARGER_AC;
+		} else {
+			printk("[FASTCHARGE] NOT set, using normal CHARGER_USB");
+			latest_chg_src = CHARGER_USB;
+		}
 		htc_batt_schedule_batt_info_update();
 		break;
 	case HTC_CHARGER_EVENT_SRC_AC: 
@@ -464,7 +474,7 @@ int htc_charger_event_notify(enum htc_charger_event event)
 								UNKNOWN_USB_DETECT_DELAY_MS)));
 		break;
 	case HTC_CHARGER_EVENT_SRC_UNKNOWN_USB: 
-		if (get_kernel_flag() & KERNEL_FLAG_ENABLE_FAST_CHARGE)
+		if ((get_kernel_flag() & KERNEL_FLAG_ENABLE_FAST_CHARGE) || force_fast_charge == 1)
 			latest_chg_src = CHARGER_AC;
 		else
 			latest_chg_src = CHARGER_UNKNOWN_USB;
